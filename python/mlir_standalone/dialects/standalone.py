38647ac14865f75f7826d4eb724f0882cd3a1104
Lorenzo Chelini <l.chelini@icloud.com>
2022-02-19 15:54:59 +0100

    Fix tablegen bindings generation for linnea

--- python/mlir_standalone/dialects/standalone.py
+++ python/mlir_standalone/dialects/standalone.py
@@ -2,5 +2,5 @@
 #  See https://llvm.org/LICENSE.txt for license information.
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from ._standalone_ops_gen import *
+from ._linnea_ops_gen import *
 from .._mlir_libs._standaloneDialects.standalone import *
