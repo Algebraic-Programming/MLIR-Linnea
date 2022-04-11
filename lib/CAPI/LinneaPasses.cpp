e426fa276ef8dd1c0cc73c46d04b4157cc0c3537
Lorenzo Chelini <l.chelini@icloud.com>
2022-04-11 17:00:48 +0200

    fix copy and paste (NFC)

--- lib/CAPI/LinneaPasses.cpp
+++ lib/CAPI/LinneaPasses.cpp
@@ -1,4 +1,4 @@
-//===- AsyncPasses.cpp - C API for Async Dialect Passes -----------------===//
+//===- LinneaPasses.cpp - C API for Linnea Dialect Passes -----------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
